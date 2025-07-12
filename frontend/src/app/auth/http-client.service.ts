82ed95d3032d2edc1b396ddb799e709cc4c0c2e1
Itapit <itamardavid90@gmail.com>
2025-07-12 21:01:51 +0300

    added env file

--- frontend/src/app/auth/http-client.service.ts
+++ frontend/src/app/auth/http-client.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { SignupDto } from './dto/signup.dto';
 import { Observable } from 'rxjs/internal/Observable';
 import { SigninDto } from './dto/signin.dto';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
-  private base = 'http://localhost:3000/auth';
-  constructor(private http: HttpClient) { }
+  private readonly base = `${environment.apiUrl}/auth`;
+  constructor(private readonly http: HttpClient) { }
 
   signup(data: SignupDto): Observable<any> {
     return this.http.post(`${this.base}/signup`, data);
